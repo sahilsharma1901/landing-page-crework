f6cad4840e6a54b2406eb0cc2e4ba8ab3d3f49e0
Sahil Sharma <sahilsharma@Sahils-MacBook-Air.local>
2022-06-02 15:58:34 +0530

    Fixes bugs

---
+++ src/data/testimonialData.js
@@ -0,0 +1,60 @@
+import DP from "../assets/images/DP.jpg";
+
+export const testimonialData = [
+    {
+        name: "Srishti Gureja",
+        batch: "ML",
+        id: 1,
+        content: "6 weeks and a wonderful experience! An area I was intrigued by but wasn't able to begin with on my own, NLP. Crework gave me a chance to not only set about learning NLP but also completing my first project. I, along with my team worked on summarisation of lecture transcripts. Cheers team, and thanks for being so supportive, made me believe I'm good at teamwork, haha!",
+        DP
+    },
+    {
+        name: "Prathamesh Dukare",
+        batch: "Web Dev",
+        id: 2,
+        content: "Today, when I look back at the day when I took the decision to fill the application for Crework, I feel that it was the best decision I took at the early stage of my career which taught me lots of things which I might have learnt later.",
+        DP
+    },
+    {
+        name: "Muskan Bagrecha",
+        batch: "ML",
+        id: 3,
+        content: "Crework is extremely close to my heart for many reasons. I was selected as one of the 8 members of Crework's very first BWU program. From designing the basic prototype to progressing towards the final deployment of the complete project, this program was an action-packed whirlwind of exploring, learning and fun. It has taught me how building within a team of like-minded and passionate people can be an experience in itself.",
+        DP
+    },
+    {
+        name: "Ishan Jain",
+        batch: "Web Dev",
+        id: 4,
+        content: "Before Crework I was a coder. After Crework I became an engineer.",
+        DP
+    },
+    {
+        name: "Garima Garg",
+        batch: "ML",
+        id: 5,
+        content: "These six weeks were the best six weeks of my college life. Apart from the amazing project that I worked upon, my major takeway was the wonderful people I met and got to interact with. Crework has not only helped me grow technically, but also as an individual. As I move forward in my Machine Learning journey, I'll always be grateful to Crework for the kickstart.",
+        DP
+    },
+    {
+        name: "Zeel Prajapati",
+        batch: "Web Dev",
+        id: 6,
+        content: "Join Crework was one the best part of my journey. I learnt many thing about product. I learn to build something which can used by others. All the mentor were very helpful and friendly. I found some my best friends in Crework.",
+        DP
+    },
+    {
+        name: "Gurjote Singh",
+        batch: "ML",
+        id: 7,
+        content: "Crework was an incredible journey, filled with so many learnings and great experiences. Apart from the projects that you do here, one thing that really stood out for me was the fact that you meet and interact with such wonderful people who have been doing great things in the Data Science space. It's like being surrounded with 9's and 10's. A huge shoutout to the whole team for pulling this off.",
+        DP
+    },
+    {
+        name: "Shofiya Bootwala",
+        batch: "Web Dev",
+        id: 8,
+        content: "Crework taught me how to build a Product and not just a project. It added an invaluable contribution to my journey of becoming a good software engineer, who builds a product with a user perspective. I joined Crework as a naive developer and in the end, I became a confident and better full stack developer.",
+        DP
+    },
+]
--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from './Hero.module.css';
 
 import prevArrow from '../../assets/icons/left-arrow.svg';
 import nextArrow from '../../assets/icons/right-arrow.svg';
-import DP from '../../assets/images/DP.jpg';
+
+import {testimonialData as data} from "../../data/testimonialData"
 
 const Hero = () => {
+
+    const [testimonials, _] = useState(data);
+    const [activeTestimonial, setActiveTestimonial] = useState(testimonials[0]);
+
+    useEffect(
+        () => {
+            const interval = setInterval(() => {
+                let activeTestimonialId = activeTestimonial.id
+                if(activeTestimonialId === testimonials.length){
+                    setActiveTestimonial(testimonials[0])
+                } else {
+                    setActiveTestimonial(testimonials[activeTestimonialId])
+                }
+            }, 4000)
+
+            return () => {
+                clearInterval(interval)
+            }
+        }, [activeTestimonial.id, testimonials]
+    )
+
   return (
     <div className={styles.heroContainer}>
         <h1 className={styles.primaryText}>Build your best <br/> project in <span className={styles.highlight}>8 weeks</span></h1>
@@ -18,21 +40,22 @@ const Hero = () => {
             <div className={styles.testimonialsCard}>
                 <div className={styles.cardHeader}>
                     <div className={styles.DP}>
-                        <img src={DP} alt="DP"/>
+                        <img src={activeTestimonial.DP} alt={activeTestimonial.name}/>
                     </div>
-                    <h2 className={styles.testimonialHeader}>Chloe Zhoe</h2>
+                    <h2 className={styles.testimonialHeader}>{activeTestimonial.name}</h2>
+                    <h3 className={styles.testimonialSubHeader}>{activeTestimonial.batch}</h3>
                 </div>
                 <div className={styles.testimonialContainer}>
-                    <p className={styles.testimonialDescription}>Previously employed at top IT Companies, Currently earns 20 LPA . Previously employed at top IT Companies, Currently earns 20 LPA .</p>
+                    <p className={styles.testimonialDescription}>{activeTestimonial.content}</p>
                     <div className={styles.navigationButtonsContainer}>
                         <p><img src={prevArrow} alt="previous"/></p>
                         <p><img src={nextArrow} alt="next"/></p>
                     </div>
                 </div>
                 <div className={styles.progressBar}>
-                    <span className={styles.one}></span>
-                    <span className={styles.two}></span>
-                    <span className={styles.three}></span>
+                    {testimonials.map((_, index) => {
+                        return <span className={`${styles.dot} ${index + 1 === activeTestimonial.id ? styles.active : ''} ${index + 1 === activeTestimonial.id - 1 ? styles.semiActive : ''}`} key={index}></span>
+                    })}
                 </div>
             </div>
         </div>

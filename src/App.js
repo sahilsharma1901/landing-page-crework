e2360c0db33322efb34e314ab48f6f94001a8891
Sahil Sharma <sharma.1901@yahoo.com>
2022-06-14 16:36:03 +0530

    Adds graduates page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
+import { Routes, Route } from "react-router-dom";
 import './App.css';
 import BWU from './pages/BWU';
 import About from './pages/About';
-import { Routes, Route } from "react-router-dom";
+import Graduates from './pages/Graduates';
 
 function App() {
   return (
     <div className='App'>
       <Routes>
         <Route path="/" exact element={<BWU />} />
-        <Route path="about" element={<About />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/graduates" element={<Graduates />} />
       </Routes>
     </div>
   );
---
+++ src/data/GraduatesData.js
@@ -0,0 +1,36 @@
+import Ishant from '../assets/images/ishant_final.jpg'
+import Sahil from '../assets/images/sahil_final.jpg'
+import Ruchika from '../assets/images/patro_final.jpeg'
+
+export const graduatesData = [
+    {
+        image: Ishant,
+        name: "Ishant Juyal",
+        batch: "Web Deb, B1",
+    },
+    {
+        image: Ruchika,
+        name: "Ruchika Patro",
+        batch: "Web Dev, B2",
+    },
+    {
+        image: Sahil,
+        name: "Sahil Sharma",
+        batch: "Web Dev, B1"
+    },
+    {
+        image: Ishant,
+        name: "Ishant Juyal",
+        batch: "ML, B1",
+    },
+    {
+        image: Ruchika,
+        name: "Ruchika Patro",
+        batch: "ML, B2",
+    },
+    {
+        image: Sahil,
+        name: "Sahil Sharma",
+        batch: "ML, B1"
+    },
+]

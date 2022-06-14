f32f57fddd667f62d5ee4380650bee7119a1c385
Sahil Sharma <sharma.1901@yahoo.com>
2022-06-14 09:58:09 +0530

    Adds about page

---
+++ src/data/teamData.js
@@ -0,0 +1,33 @@
+import Ishant from '../assets/images/ishant_final.jpg'
+import Sahil from '../assets/images/sahil_final.jpg'
+import Ruchika from '../assets/images/patro_final.jpeg'
+
+export const teamData = [
+    {
+        image: Ishant,
+        name: "Ishant Juyal",
+        designation: "Co-founder",
+        socials: [
+            "https://twitter.com/juyal_ishant",
+            "https://www.linkedin.com/in/ishantjuyal/"
+        ]
+    },
+    {
+        image: Ruchika,
+        name: "Ruchika Patro",
+        designation: "Community",
+        socials: [
+            "https://twitter.com/Sneha_Ruchi",
+            "https://www.linkedin.com/in/patroruchika/"
+        ]
+    },
+    {
+        image: Sahil,
+        name: "Sahil Sharma",
+        designation: "Co-founder",
+        socials: [
+            "https://twitter.com/sahilasharma19",
+            "https://www.linkedin.com/in/sahilasharma/"
+        ]
+    },
+]
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import styles from './About.module.css'
+import {teamData as data} from "../data/teamData"
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className={styles.aboutContainer}>
+        <h1 className={styles.heading}>About us</h1>
+        <p className={styles.content}>
+        At Crework, we believe that everyone deserves better. Started as a WhatsApp group, today we are known as a place to meet, learn and build with some of the smartest people in tech.
+        </p>
+        <div className={styles.contentWrapper}>
+          <h3 className={styles.subHeading}>How it started?</h3>
+          <p className={styles.content}>The idea of Crework was pretty simple. Students want to build projects, but building a project alone has its own limitation. You need people to work with. You need better people, people you can learn with and people who want you to succeed. But, finding a peer group like this is hard.</p>
+          <br/>
+          <p className={styles.content}>So, we decided to make it easy. That’s how Crework batches were born. The first batch was just a few mentors helping 8 students trying to build Machine Learning projects. We tried it for the first time in 2021 and it worked. It worked, and we knew we have to keep doing this.</p>
+        </div>
+        <div className={styles.contentWrapper}>
+          <h3 className={styles.subHeading}>What happens in Crework?</h3>
+          <p className={styles.content}>In Crework, you learn to build products. We have been building an ecosystem where it is easier to ask questions, get personalised guidance from industry mentors and build projects with other people in the community over a fixed period of time.</p>
+          <br/>
+          <p className={styles.content}>After successful BWU batches, we intend to leverage the community and this ecosystem to replicate the process of learning by building projects in the beginner cohorts too.</p>
+        </div>
+        <div className={styles.contentWrapper}>
+          <h3 className={`${styles.subHeading} ${styles.center}`}>Meet the team</h3>
+          <div className={styles.teamContainer}>
+            {data.map((member, index) => {
+              return (
+                <div className={styles.card} key={index}>
+                  <img src={member.image} alt={member.name} className={styles.image}/>
+                  <div className={styles.infoContainer}>
+                    <div className={styles.infoInner}>
+                      <h4 className={styles.name}>{member.name}</h4>
+                      <h4 className={styles.role}>{member.designation}</h4>
+                    </div>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+    </div>
   )
 }
 

01bc95f3472caba72dced17bbc25ca86531ad338
Rizwan Memon <rizwan2000.rm@gmail.com>
2022-06-19 14:07:17 +0530

    fixes desktop card grid, know more mobile

--- src/sections/Beginner_SubCTA/SubCTA.jsx
+++ src/sections/Beginner_SubCTA/SubCTA.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import styles from "./SubCTA.module.css"
+import React from "react";
+import styles from "./SubCTA.module.css";
+import Arrow from "../../assets/icons/right-arrow.png";
 
 const SubCTA = () => {
   return (
     <div className={styles.subCTAContainer}>
-        <a href="https://crework.notion.site/crework/Beginner-Cohort-c903bdea287d4106829c5014c6868b8a" target="_blank" rel="noreferrer" className={styles.heading}><span className={styles.first}>Know more about the cohort here <span>→</span></span></a>
+      <a
+        href="https://crework.notion.site/crework/Beginner-Cohort-c903bdea287d4106829c5014c6868b8a"
+        target="_blank"
+        rel="noreferrer"
+        className={styles.heading}
+      >
+        <span className={styles.first}>
+          Know more about the cohort here
+          <img src={Arrow} alt="" />
+        </span>
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default SubCTA
+export default SubCTA;

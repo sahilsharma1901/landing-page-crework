3b325dd85a5e48704bf00cacccafbb3ac5ed246b
Sahil Sharma <sharma.1901@yahoo.com>
2022-06-29 21:59:24 +0530

    Works on feedback

--- src/sections/Parent_Hero/ParentHero.jsx
+++ src/sections/Parent_Hero/ParentHero.jsx
@@ -10,9 +10,9 @@ const ParentHero = () => {
     const navigate = useNavigate()
     const handleProgramSelection = (page) => {
         if(page === 'BWU'){
-            navigate("/bwu", {replace: true})
+            navigate("/bwu", {replace: false})
         } else if(page === 'Beginner'){
-            navigate("/beginners", {replace: true})
+            navigate("/beginners", {replace: false})
         }
     }
 
@@ -21,15 +21,17 @@ const ParentHero = () => {
         <div className={styles.section}>
             <h1 className={styles.primaryText}>Community for the best in tech and product</h1>
             <h2 className={styles.secondaryText}>On a mission to create and enable builders</h2>
-            <button className={styles.cta} onClick={handleProgramSelection.bind(null, 'Beginner')}>Join Crework</button>
+            <button className={styles.cta} onClick={handleProgramSelection.bind(null, 'Beginner')}>Learn More</button>
         </div>
         <div className={styles.programsContainer} id="programs">
             <h2 className={styles.pHeading}>Our Programs</h2>
             <div className={styles.programs}>
                 <div className={styles.program}  onClick={handleProgramSelection.bind(null, 'BWU')}>
+                    <p className={styles.label}>BWU</p>
                     <img src={BWU} alt="BWU Batch"/>
                 </div>
                 <div className={styles.program} onClick={handleProgramSelection.bind(null, 'Beginner')}>
+                    <p className={styles.label}>Beginner</p>
                     <img src={Beginner} alt="Beginner's Cohort"/>
                 </div>
             </div>

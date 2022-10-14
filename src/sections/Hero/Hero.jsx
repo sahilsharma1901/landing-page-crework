407e3802220c06022ec126248f1590539a6a3263
Barun Debnath <barundebnath91@gmail.com>
2022-10-14 22:04:40 +0530

    components changed

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import styles from "./Hero.module.css";
 
 import prevArrow from "../../assets/icons/left-arrow.svg";
@@ -50,77 +50,79 @@ const Hero = () => {
 
   return (
     <div className={styles.heroContainer}>
-      <h1 className={styles.primaryText}>
-        Build <span className={styles.specialText}>&nbsp;and Launch</span>{" "}
-        <br />
-        your best project
-        <span className={styles.highlight}>
-          {" "}
-          <br /> in 12 weeks
-        </span>
-      </h1>
-      <p className={styles.secondaryText}>
-        Ideate, build, network and get mentored <br /> with the best people in
-        tech
-      </p>
-      <div className={styles.heroInnerContainer}>
-        <div className={styles.CTAContainer}>
-          <a
-            href="https://tally.so/r/3jaQoE"
-            target="_blank"
-            rel="noreferrer"
-            className={styles.joinWaitlist}>
-            Join Waitlist
-          </a>
-          <a href="#knowMore" className={styles.knowMore}>
-            Know more
-          </a>
-        </div>
-        <div className={styles.testimonialsCard}>
-          <div className={styles.cardHeader}>
-            <div className={styles.DP}>
-              <img src={activeDP} alt={activeTestimonial.name} />
-            </div>
-            <h2 className={styles.testimonialHeader}>
-              <span className={styles.p_text}>{activeTestimonial.name}</span>
-              <span className={styles.s_text}>
-                {activeTestimonial.batch} batch
-              </span>
-            </h2>
+      <div className="">
+        <h1 className={styles.primaryText}>
+          Build <span className={styles.specialText}>&nbsp;and Launch</span>{" "}
+          <br />
+          your best project
+          <span className={styles.highlight}>
+            {" "}
+            <br /> in 12 weeks
+          </span>
+        </h1>
+        <p className={styles.secondaryText}>
+          Ideate, build, network and get mentored <br /> with the best people in
+          tech
+        </p>
+        <div className={styles.heroInnerContainer}>
+          <div className={styles.CTAContainer}>
+            <a
+              href="https://tally.so/r/3jaQoE"
+              target="_blank"
+              rel="noreferrer"
+              className={styles.joinWaitlist}>
+              Join Waitlist
+            </a>
+            <a href="#knowMore" className={styles.knowMore}>
+              Know more
+            </a>
           </div>
-          <div className={styles.testimonialContainer}>
-            <p className={styles.testimonialDescription}>
-              {activeTestimonial.content}
+        </div>
+      </div>
+      <div className={styles.testimonialsCard}>
+        <div className={styles.cardHeader}>
+          <div className={styles.DP}>
+            <img src={activeDP} alt={activeTestimonial.name} />
+          </div>
+          <h2 className={styles.testimonialHeader}>
+            <span className={styles.p_text}>{activeTestimonial.name}</span>
+            <span className={styles.s_text}>
+              {activeTestimonial.batch} batch
+            </span>
+          </h2>
+        </div>
+        <div className={styles.testimonialContainer}>
+          <p className={styles.testimonialDescription}>
+            {activeTestimonial.content}
+          </p>
+          <div className={styles.progressBar}>
+            {testimonials.map((_, index) => {
+              return (
+                <span
+                  className={`${styles.dot} ${
+                    index + 1 === activeTestimonial.id ? styles.active : ""
+                  } ${
+                    index + 1 === activeTestimonial.id - 1
+                      ? styles.semiActive
+                      : ""
+                  }`}
+                  key={index}></span>
+              );
+            })}
+          </div>
+          <div className="filler"></div>
+          <div className={styles.navigationButtonsContainer}>
+            <p>
+              <img
+                src={nextArrow}
+                alt="previous"
+                onClick={handleDecrement}
+                className={styles.reversedImage}
+              />
             </p>
-            <div className={styles.progressBar}>
-              {testimonials.map((_, index) => {
-                return (
-                  <span
-                    className={`${styles.dot} ${
-                      index + 1 === activeTestimonial.id ? styles.active : ""
-                    } ${
-                      index + 1 === activeTestimonial.id - 1
-                        ? styles.semiActive
-                        : ""
-                    }`}
-                    key={index}></span>
-                );
-              })}
-            </div>
-            <div className="filler"></div>
-            <div className={styles.navigationButtonsContainer}>
-              <p>
-                <img
-                  src={nextArrow}
-                  alt="previous"
-                  onClick={handleDecrement}
-                  className={styles.reversedImage}
-                />
-              </p>
-              <p>
-                <img src={nextArrow} alt="next" onClick={handleIncrement} />
-              </p>
-            </div>
+            <p>
+              <img src={nextArrow} alt="next" onClick={handleIncrement} />
+            </p>
           </div>
         </div>
       </div>

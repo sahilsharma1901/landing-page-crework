6cf50b23038d03c226faa2520d4dce469d965237
Barun Debnath <barundebnath91@gmail.com>
2022-10-14 21:27:58 +0530

    animation removed

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,90 +1,130 @@
-import React, {useEffect, useState, useCallback} from 'react';
-import styles from './Hero.module.css';
+import React, { useCallback, useEffect, useState } from "react";
+import styles from "./Hero.module.css";
 
-import prevArrow from '../../assets/icons/left-arrow.svg';
-import nextArrow from '../../assets/icons/right-arrow.svg';
+import prevArrow from "../../assets/icons/left-arrow.svg";
+import nextArrow from "../../assets/icons/right-arrow.svg";
 
-import {testimonialData as data} from "../../data/testimonialData"
+import { testimonialData as data } from "../../data/testimonialData";
 
 const Hero = () => {
-    const [testimonials, _] = useState(data);
-    const [activeIndex, setActiveIndex] = useState(0)
-    const [activeTestimonial, setActiveTestimonial] = useState(testimonials[activeIndex]);
-    const [activeDP, setActiveDP] = useState(activeTestimonial.DP)
+  const [testimonials, _] = useState(data);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [activeTestimonial, setActiveTestimonial] = useState(
+    testimonials[activeIndex],
+  );
+  const [activeDP, setActiveDP] = useState(activeTestimonial.DP);
 
-    const handleIncrement = useCallback(() => {
-        if(activeIndex === testimonials.length - 1){
-            setActiveIndex(0)
-            setActiveTestimonial(testimonials[0])
-            setActiveDP(testimonials[0].DP)
-        } else {
-            setActiveIndex(activeIndex + 1)
-            setActiveTestimonial(testimonials[activeIndex + 1])
-            setActiveDP(testimonials[activeIndex + 1].DP)
-        }
-    }, [activeIndex, testimonials])
-
-    useEffect(
-        () => {
-            const interval = setInterval(() => {
-                handleIncrement()
-            }, 8000)
-
-            return () => {
-                clearInterval(interval)
-            }
-        }, [handleIncrement]
-    )
-
-
-    const handleDecrement = () => {
-        if(activeIndex <= 0){
-            setActiveIndex(testimonials.length - 1)
-            setActiveTestimonial(testimonials[testimonials.length - 1])
-            setActiveDP(testimonials[testimonials.length - 1].DP)
-        } else {
-            setActiveIndex(activeIndex - 1)
-            setActiveTestimonial(testimonials[activeIndex - 1])
-            setActiveDP(testimonials[activeIndex - 1].DP)
-        }
+  const handleIncrement = useCallback(() => {
+    if (activeIndex === testimonials.length - 1) {
+      setActiveIndex(0);
+      setActiveTestimonial(testimonials[0]);
+      setActiveDP(testimonials[0].DP);
+    } else {
+      setActiveIndex(activeIndex + 1);
+      setActiveTestimonial(testimonials[activeIndex + 1]);
+      setActiveDP(testimonials[activeIndex + 1].DP);
     }
+  }, [activeIndex, testimonials]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      handleIncrement();
+    }, 8000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [handleIncrement]);
+
+  const handleDecrement = () => {
+    if (activeIndex <= 0) {
+      setActiveIndex(testimonials.length - 1);
+      setActiveTestimonial(testimonials[testimonials.length - 1]);
+      setActiveDP(testimonials[testimonials.length - 1].DP);
+    } else {
+      setActiveIndex(activeIndex - 1);
+      setActiveTestimonial(testimonials[activeIndex - 1]);
+      setActiveDP(testimonials[activeIndex - 1].DP);
+    }
+  };
 
   return (
     <div className={styles.heroContainer}>
-        <h1 className={styles.primaryText}>Build <span className={styles.specialText}>&nbsp;and Launch</span>  <br/>your best project<span className={styles.highlight}> <br/> in 12 weeks</span></h1>
-        <p className={styles.secondaryText}>Ideate, build, network and get mentored <br/> with the best people in tech</p>
-        <div className={styles.heroInnerContainer}>
-            <div className={styles.CTAContainer}>
-                <a href="https://tally.so/r/3jaQoE" target="_blank" rel="noreferrer" className={styles.joinWaitlist}>Join Waitlist</a>
-                <a href="#knowMore" className={styles.knowMore}>Know more</a>
-            </div>
-            <div className={styles.testimonialsCard}>
-                <div className={styles.cardHeader}>
-                    <div className={styles.DP}>
-                        <img src={activeDP} alt={activeTestimonial.name}/>
-                    </div>
-                    <h2 className={styles.testimonialHeader}>
-                        <span className={styles.p_text}>{activeTestimonial.name}</span>
-                        <span className={styles.s_text}>{activeTestimonial.batch} batch</span>
-                    </h2>
-                </div>
-                <div className={styles.testimonialContainer}>
-                    <p className={styles.testimonialDescription}>{activeTestimonial.content}</p>
-                    <div className={styles.progressBar}>
-                        {testimonials.map((_, index) => {
-                            return <span className={`${styles.dot} ${index + 1 === activeTestimonial.id ? styles.active : ''} ${index + 1 === activeTestimonial.id - 1 ? styles.semiActive : ''}`} key={index}></span>
-                        })}
-                    </div>
-                    <div className="filler"></div>
-                    <div className={styles.navigationButtonsContainer}>
-                        <p><img src={nextArrow} alt="previous" onClick={handleDecrement} className={styles.reversedImage}/></p>
-                        <p><img src={nextArrow} alt="next" onClick={handleIncrement}/></p>
-                    </div>
-                </div>
-            </div>
+      <h1 className={styles.primaryText}>
+        Build <span className="">and Launch</span> <br />
+        your best project
+        <span className={styles.highlight}>
+          {" "}
+          <br /> in 12 weeks
+        </span>
+      </h1>
+      <p className={styles.secondaryText}>
+        Ideate, build, network and get mentored <br /> with the best people in
+        tech
+      </p>
+      <div className={styles.heroInnerContainer}>
+        <div className={styles.CTAContainer}>
+          <a
+            href="https://tally.so/r/3jaQoE"
+            target="_blank"
+            rel="noreferrer"
+            className={styles.joinWaitlist}>
+            Join Waitlist
+          </a>
+          <a href="#knowMore" className={styles.knowMore}>
+            Know more
+          </a>
         </div>
+        <div className={styles.testimonialsCard}>
+          <div className={styles.cardHeader}>
+            <div className={styles.DP}>
+              <img src={activeDP} alt={activeTestimonial.name} />
+            </div>
+            <h2 className={styles.testimonialHeader}>
+              <span className={styles.p_text}>{activeTestimonial.name}</span>
+              <span className={styles.s_text}>
+                {activeTestimonial.batch} batch
+              </span>
+            </h2>
+          </div>
+          <div className={styles.testimonialContainer}>
+            <p className={styles.testimonialDescription}>
+              {activeTestimonial.content}
+            </p>
+            <div className={styles.progressBar}>
+              {testimonials.map((_, index) => {
+                return (
+                  <span
+                    className={`${styles.dot} ${
+                      index + 1 === activeTestimonial.id ? styles.active : ""
+                    } ${
+                      index + 1 === activeTestimonial.id - 1
+                        ? styles.semiActive
+                        : ""
+                    }`}
+                    key={index}></span>
+                );
+              })}
+            </div>
+            <div className="filler"></div>
+            <div className={styles.navigationButtonsContainer}>
+              <p>
+                <img
+                  src={nextArrow}
+                  alt="previous"
+                  onClick={handleDecrement}
+                  className={styles.reversedImage}
+                />
+              </p>
+              <p>
+                <img src={nextArrow} alt="next" onClick={handleIncrement} />
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Hero;
